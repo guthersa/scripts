9dd009a0590d87ca179f1a765b31cdff03eee583
guthersa <34872239+guthersa@users.noreply.github.com>
2017-12-30 23:06:33 -0200

    Add files via upload

---
+++ login.sh
@@ -0,0 +1,16 @@
+/usr/bin/expect -f - <<EOD
+spawn heroku login
+expect "Email: "
+send "$1\r"
+expect "Password: "
+sleep 1
+send "$2"
+sleep 1
+send "\n"
+expect "Logged in"
+sleep 2
+spawn heroku container:login
+sleep 1
+expect "Login Succeeded"
+sleep 1
+EOD
---
+++ push.sh
@@ -0,0 +1,5 @@
+heroku container:push web --app $1 &&
+heroku container:push web --app $2 &&
+heroku container:push web --app $3 &&
+heroku container:push web --app $4 &&
+heroku container:push web --app $5
